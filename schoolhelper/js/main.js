9a8e23ace506e07402b3016a8282a9ff87744c25
AlexCookie <unconfigured@null.spigotmc.org>
2018-05-18 00:26:34 +0300

    Исключение пустого места

--- schoolhelper/js/main.js
+++ schoolhelper/js/main.js
@@ -56,23 +56,25 @@ function CalculateFix() {
 
   console.log(fourfour+" "+fivefour+" "+fivefive)
   console.log("--------------------------------")
-  if (avgscore<3.5 ) {
-    document.getElementById("fives").innerHTML = "До четвёрки нужно получить еще "+fivefour+" "+wordfive1+" или "+fourfour+" "+wordfour1+"!";
-    document.getElementById("mark").style.backgroundColor = "#3a3a3a";
-    document.getElementById("mark").style.color = "#b1b1b1";
-  }else {
-    document.getElementById("fives").innerHTML = "Средний балл больше 3.5!";
-    document.getElementById("mark").style.backgroundColor = "#c2d23a";
-    document.getElementById("mark").style.color = "#fff";
-  }
+  if (ine.value = "") {
+    if (avgscore<3.5 ) {
+      document.getElementById("fives").innerHTML = "До четвёрки нужно получить еще "+fivefour+" "+wordfive1+" или "+fourfour+" "+wordfour1+"!";
+      document.getElementById("mark").style.backgroundColor = "#3a3a3a";
+      document.getElementById("mark").style.color = "#b1b1b1";
+    }else {
+      document.getElementById("fives").innerHTML = "Средний балл больше 3.5!";
+      document.getElementById("mark").style.backgroundColor = "#c2d23a";
+      document.getElementById("mark").style.color = "#fff";
+    }
 
-  if (avgscore<4.5) {
-    document.getElementById("fives2").innerHTML = "До пятерки нужно получить еще "+fivefive+" "+wordfive2+"!";
-    document.getElementById("mark2").style.backgroundColor = "#3a3a3a";
-    document.getElementById("mark2").style.color = "#b1b1b1";
-  }else {
-    document.getElementById("fives2").innerHTML = "Средний балл больше 4.5!";
-    document.getElementById("mark2").style.backgroundColor = "#c2d23a";
-    document.getElementById("mark2").style.color = "#fff";
+    if (avgscore<4.5) {
+      document.getElementById("fives2").innerHTML = "До пятерки нужно получить еще "+fivefive+" "+wordfive2+"!";
+      document.getElementById("mark2").style.backgroundColor = "#3a3a3a";
+      document.getElementById("mark2").style.color = "#b1b1b1";
+    }else {
+      document.getElementById("fives2").innerHTML = "Средний балл больше 4.5!";
+      document.getElementById("mark2").style.backgroundColor = "#c2d23a";
+      document.getElementById("mark2").style.color = "#fff";
+    }
   }
 }
